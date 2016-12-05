f0e7a9b5e3f25f55338ab785df136c6c9236c22d
hong007 <hongty@hong007deMac-mini.local>
2016-12-05 14:04:00 +0800

    erge dev

--- android/app/src/main/java/com/jetgo/MainApplication.java
+++ android/app/src/main/java/com/jetgo/MainApplication.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.eguma.barcodescanner.BarcodeScannerPackage;
-import com.eguma.barcodescanner.BarcodeScannerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
